3a169ed793654b39a128197d96c3f8016daa928e
Anthony Leclercq <aleclercq59.dev@gmail.com>
2024-03-20 01:10:36 +0100

    Challenge Cards

---
+++ app/lesson/_components/challenge.tsx
@@ -0,0 +1,48 @@
+import { cn } from "@/lib/utils";
+import { CHALLENGE_TYPE, ChallengeOption } from "@prisma/client";
+import { Card } from "./card";
+
+type Props = {
+  options: ChallengeOption[];
+  onSelect: (id: string) => void;
+  status: "correct" | "wrong" | "none";
+  selectedOption?: string;
+  disabled?: boolean;
+  type: CHALLENGE_TYPE;
+};
+
+export const Challenge = ({
+  options,
+  onSelect,
+  status,
+  selectedOption,
+  disabled,
+  type,
+}: Props) => {
+  return (
+    <div
+      className={cn(
+        "grid gap-2",
+        type === "ASSIST" && "grid-cols-1",
+        type === "SELECT" &&
+          "grid-cols-2 lg:grid-cols-[repeat(auto-fit,minmax(0,1fr))]"
+      )}
+    >
+      {options.map((option, i) => (
+        <Card
+          key={option.id}
+          id={option.id}
+          text={option.text}
+          imageSrc={option.imageSrc}
+          shortcut={`${i + 1}`}
+          selected={selectedOption === option.id}
+          onClick={() => onSelect(option.id)}
+          status={status}
+          audioSrc={option.audioSrc}
+          disabled={disabled}
+          type={type}
+        />
+      ))}
+    </div>
+  );
+};
